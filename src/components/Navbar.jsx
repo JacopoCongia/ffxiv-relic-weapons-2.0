b5ad93b136e45368adc3c3d15dd53d8f7d5a198f
jacopo.congia <jacopo.congia@gmail.com>
2023-08-02 08:51:54 +0200

    Updated Header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar({ isVisible, setIsVisible }) {
   return (
     <div
       onClick={() => setIsVisible(!isVisible)}
-      className="drop-shadow-md sticky top-0 z-10 select-none items-center bg-[#1f1f1f] px-5 py-7 text-center font-bold  tracking-wider text-white"
+      className="drop-shadow-md sticky top-0 z-10 select-none items-center bg-[#1f1f1f] px-5 py-7 text-center font-bold  tracking-wider text-white min-[1000px]:text-left"
     >
       {!isVisible ? (
         <RxHamburgerMenu className="absolute top-[50%] translate-y-[-50%] cursor-pointer text-[1.8rem] hover:opacity-90 min-[1000px]:hidden" />
@@ -15,7 +15,7 @@ function Navbar({ isVisible, setIsVisible }) {
       )}
       <Link
         to="/"
-        className="m-auto text-[1rem] min-[520px]:m-0 min-[520px]:text-[1.5rem]"
+        className="text-[1rem] min-[520px]:m-0 min-[520px]:text-[1.5rem]"
       >
         Final Fantasy XIV{" "}
         <span className="block text-[1.5rem] min-[520px]:inline">
