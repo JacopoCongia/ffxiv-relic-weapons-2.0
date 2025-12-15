d0e4296f534a4df8530a474221c0f3d3ea34be40
jacopo.congia <jacopo.congia@gmail.com>
2025-12-15 08:47:59 +0100

    Add Dawntrail umbrae icons and materials, update data
    Added new material and umbrae icons for Dawntrail to the public/icons directory. Updated data.js and materialsData.js to include new Dawntrail materials and weapons. Modified Materials, MaterialsContainer, and Dawntrail components to support the new data.

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -1,30 +1,30 @@
-function Materials({ materials, counter }) {
-  const materialsElements = materials.map((material) => {
-    return (
-      <div
-        key={material.id}
-        className="flex items-center gap-3"
-      >
-        <img
-          className="w-5"
-          alt={material.name}
-          src={material.icon}
-        />
-        {material.reqTotal - material.reqPerWeapon * counter.length <= 0 ? (
-          <p>
-            {material.name}: {0}
-          </p>
-        ) : (
-          <p>
-            {material.name}:{" "}
-            {material.reqTotal - material.reqPerWeapon * counter.length}
-          </p>
-        )}
-      </div>
-    );
-  });
+function Materials({materials, counter}) {
+    const materialsElements = materials.map((material) => {
+        return (
+            <div
+                key={material.id}
+                className="flex items-center gap-3"
+            >
+                <img
+                    className="w-9 rounded-[50%]"
+                    alt={material.name}
+                    src={material.icon}
+                />
+                {material.reqTotal - material.reqPerWeapon * counter.length <= 0 ? (
+                    <p>
+                        {material.name}: {0}
+                    </p>
+                ) : (
+                    <p>
+                        {material.name}:{" "}
+                        {material.reqTotal - material.reqPerWeapon * counter.length}
+                    </p>
+                )}
+            </div>
+        );
+    });
 
-  return <>{materialsElements}</>;
+    return <>{materialsElements}</>;
 }
 
 export default Materials;
--- src/pages/Dawntrail.jsx
+++ src/pages/Dawntrail.jsx
@@ -4,23 +4,33 @@ import Header from "../components/Header";
 import WeaponsSection from "../components/WeaponsSection";
 
 function Dawntrail() {
-  const { weapons, ownedWeapons } = useWeaponsData();
+    const {weapons, ownedWeapons} = useWeaponsData();
 
-  return (
-    <div className="w-[100%] duration-[0.5s] min-[1000px]:pl-[250px]">
-      <Header title="Phantom" />
-      <WeaponsSection
-        weapons={weapons.penumbrae}
-        ownedWeapons={ownedWeapons}
-        name="Penumbrae"
-        category="penumbrae"
-        patchInfo="iLvl 745 (Patch 7.25)"
-        materials={materialsData.materials.dawntrail.dow.penumbrae}
-        tomestones={materialsData.tomestones.heliometry}
-        tomestoneAmount={1500}
-      />
-    </div>
-  );
+    return (
+        <div className="w-[100%] duration-[0.5s] min-[1000px]:pl-[250px]">
+            <Header title="Phantom"/>
+            <WeaponsSection
+                weapons={weapons.penumbrae}
+                ownedWeapons={ownedWeapons}
+                name="Penumbrae"
+                category="penumbrae"
+                patchInfo="iLvl 745 (Patch 7.25)"
+                materials={materialsData.materials.dawntrail.dow.penumbrae}
+                tomestones={materialsData.tomestones.heliometry}
+                tomestoneAmount={1500}
+            />
+            <WeaponsSection
+                weapons={weapons.umbrae}
+                ownedWeapons={ownedWeapons}
+                name="Umbrae"
+                category="umbrae"
+                patchInfo="iLvl 760 (Patch 7.35)"
+                materials={materialsData.materials.dawntrail.dow.umbrae}
+                tomestones={materialsData.tomestones.heliometry}
+                tomestoneAmount={1500}
+            />
+        </div>
+    );
 }
 
 export default Dawntrail;
